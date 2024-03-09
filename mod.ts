8d3d27e7830eaf751f07a5ac2aa2eec58c75a27e
David Bushell <david@dbushell.com>
2024-03-09 10:30:59 +0000

    deploy fixes

--- mod.ts
+++ mod.ts
@@ -1,5 +1,14 @@
-import {DinoSsr} from 'dinossr';
+import {DinoSsr, type DinoManifest} from 'dinossr';
 
-const dinossr = new DinoSsr();
+const dir = new URL('./', import.meta.url).pathname;
+
+let manifest: DinoManifest | undefined;
+if (Deno.env.has('DENO_REGION')) {
+  manifest = await import('file:///src/.dinossr/manifest.js');
+}
+
+const dinossr = new DinoSsr(dir, {
+  manifest
+});
 
 await dinossr.init();
